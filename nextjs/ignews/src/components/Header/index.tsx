5c2c4b67ad0c57ca6b08a3c9ebabe438b2b3c0e0
EdersonDav <silva.edersonqueiroz@gmail.com>
2021-07-07 09:06:31 -0300

    create Header

---
+++ nextjs/ignews/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+import styles from './styles.module.scss';
+import { SignInButton } from '../SignInButton/index';
+
+export const Header = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+};
