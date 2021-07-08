347c6b0d4570a81ab6d229812c90f474d4bc7c04
EdersonDav <silva.edersonqueiroz@gmail.com>
2021-07-08 08:53:43 -0300

    create home page

--- nextjs/ignews/src/pages/index.tsx
+++ nextjs/ignews/src/pages/index.tsx
@@ -1,11 +1,31 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
+
+import styles from './home.module.scss';
+import { SubscribeButton } from '../components/SubscribeButton/index';
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
-      <h1>Hello</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋🏿 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
